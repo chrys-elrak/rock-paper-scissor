bf23108d2da91971db536a58254f23e4aaa25838
Johannes Maria Frank <jmfrank63@gmail.com>
2022-10-09 10:15:35 +0100

    fix bug paper vs scissors

--- src/helpers/get_winner.rs
+++ src/helpers/get_winner.rs
@@ -3,16 +3,17 @@ use crate::enums::choice::Choice::{*, self};
 * First return is for user, the last for computer
 */
 pub fn winner(user_choice: Choice, computer_choice: Choice) -> (bool, bool) {
-    let res = if user_choice == ROCK && computer_choice == PAPER
-        || user_choice == SCISSOR && computer_choice == ROCK
-    {
-        (false, true)
-    } else if user_choice == computer_choice {
-        (true, true)
-    } else {
-        (true, false)
-    };
-    return res;
+    match (user_choice, computer_choice) {
+        (ROCK, SCISSOR) => (true, false),
+        (SCISSOR, ROCK) => (false, true),
+        (SCISSOR, PAPER) => (true, false),
+        (PAPER, SCISSOR) => (false, true),
+        (PAPER, ROCK) => (true, false),
+        (ROCK, PAPER) => (false, true),
+        (SCISSOR, SCISSOR) => (true, true),
+        (ROCK, ROCK) => (true, true),
+        (PAPER, PAPER) => (true, true),
+    }
 }
 
 #[cfg(test)]
