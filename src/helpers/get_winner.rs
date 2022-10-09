ca7b7c08a73cdc7c75b1d7a9c3d47ed7ac54f545
Johannes Maria Frank <jmfrank63@gmail.com>
2022-10-09 10:04:28 +0100

    Adding winner function test suite

--- src/helpers/get_winner.rs
+++ src/helpers/get_winner.rs
@@ -14,3 +14,42 @@ pub fn winner(user_choice: Choice, computer_choice: Choice) -> (bool, bool) {
     };
     return res;
 }
+
+#[cfg(test)]
+#[test]
+fn user_rock_sharpens_sissors() {
+    assert_eq!(winner(ROCK, SCISSOR), (true, false));
+}
+
+#[test]
+fn computer_rock_sharpens_sissors() {
+    assert_eq!(winner(SCISSOR, ROCK), (false, true));
+}
+
+#[test]
+fn user_scissors_cut_paper() {
+    assert_eq!(winner(SCISSOR, PAPER), (true, false));
+   
+}
+
+#[test]
+fn computer_scissors_cut_paper() {
+    assert_eq!(winner(PAPER, SCISSOR), (false, true));
+}
+
+#[test]
+fn user_paper_wraps_rock() {
+    assert_eq!(winner(PAPER, ROCK), (true, false));
+}
+
+#[test]
+fn computer_paper_wraps_rock() {
+    assert_eq!(winner(ROCK, PAPER), (false, true));
+}
+
+#[test]
+fn same_choice_is_draw() {
+    assert_eq!(winner(ROCK, ROCK), (true, true));
+    assert_eq!(winner(PAPER, PAPER), (true, true));
+    assert_eq!(winner(SCISSOR, SCISSOR), (true, true));
+}
