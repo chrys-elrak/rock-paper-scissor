f61095393de2eb0d3d424cfc326c0309a89f1d39
Johannes Maria Frank <jmfrank63@gmail.com>
2022-10-11 23:25:28 +0100

    add scoring and bug fixes

--- src/main.rs
+++ src/main.rs
@@ -9,43 +9,50 @@ use crate::helpers::get_input::input;
 use crate::helpers::get_winner::{message, winner};
 use crate::helpers::show_loader::loading;
 
+#[derive(Default, Debug)]
+struct Stats {
+    user: usize,
+    computer: usize,
+    draws: usize,
+}
+
 fn main() {
-    let mut score = 0;
+    let mut stats = Stats::default();
     loop {
         let header: colored::ColoredString = "
-    (1): Rock (default)
+    (1): Rock
     (2): Paper
     (3): Scissor
     (4): Spock
     (5): Lizard
+    Any other key to end the game
     "
         .yellow();
         let user_choice;
         println!("{}", header);
         let s = input("Please choose");
-        user_choice = choice(&s).unwrap_or_default();
-        let x = rand::thread_rng().gen_range(1..4);
+        user_choice = match choice(&s) {
+            Some(choice) => choice,
+            None => {
+                println!("No choice given, ending game.");
+                break
+            }
+        };
+        let x = rand::thread_rng().gen_range(1..6);
+        println!("{}", x);
         let computer_choice = choice(x.to_string().as_str()).unwrap();
         loading("Loading...", 100);
         let s1 = "\nYou choose:".cyan();
         let s2 = "I choose:".blue();
-        println!("{} {:?}", s1, user_choice.clone());
-        println!("{} {:?}", s2, computer_choice.clone());
-        let win = winner(user_choice, computer_choice);
-        match win {
-            (true, false) => score += 1,
-            (false, true) => score -= 1,
-            _ => (),
+        println!("{} {:?}", &s1, &user_choice);
+        println!("{} {:?}", &s2, &computer_choice);
+        match winner(&user_choice, &computer_choice) {
+            Some(true) => stats.user += 1,
+            Some(false) => stats.computer += 1,
+            None => stats.draws += 1,
         }
-        message(win);
-        if score <= 0 {
-            println!("You lost the game !");
-            score = 0;
-        }
-        let s = input("Do you want to play again ? (Y/n)");
-        if s.to_lowercase() == "n" {
-            println!("Your score: {}", score);
-            break;
-        }
+        message(winner(&user_choice, &computer_choice));
     }
+    println!("{:?}", stats);
+
 }
