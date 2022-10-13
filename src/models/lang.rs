da935afb65703711e0ba3df040ab18e65ab33625
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-14 00:23:48 +0300

    language systems

---
+++ src/models/lang.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use serde::{Deserialize};
+
+pub enum AvalaibleLang {
+    EN,
+    FR
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub struct Lang {
+    pub rock: String,
+    pub paper: String,
+    pub scissors: String,
+    pub spock: String,
+    pub lizard: String,
+    pub exit: String,
+}
+
+impl Lang {
+    pub fn new(lang: AvalaibleLang) -> Self {
+        let file = match lang {
+            AvalaibleLang::EN => "src/lang/en.json",
+            AvalaibleLang::FR => "src/lang/fr.json",
+        };
+        let file = fs::read_to_string(file).expect("Something went wrong reading the file");
+        let l: Lang = serde_json::from_str(&file).expect("Something went wrong parsing the file");
+        l
+    }
+}
