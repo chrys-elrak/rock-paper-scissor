e2f7e79323845c41765b85f59cc480fcfd69a780
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-13 23:58:33 +0300

    implements display trait

--- src/enums/choice.rs
+++ src/enums/choice.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Formatter, Display};
+
 #[derive(Debug)]
 #[derive(PartialEq)]
 #[derive(Clone)]
@@ -13,4 +15,16 @@ impl  Default for Choice {
     fn default() -> Self {
         Choice::ROCK
     }
+}
+
+impl Display for Choice {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Choice::ROCK => write!(f, "1"),
+            Choice::PAPER => write!(f, "2"),
+            Choice::SCISSOR => write!(f, "3"),
+            Choice::SPOCK => write!(f, "4"),
+            Choice::LIZARD => write!(f, "5"),
+        }
+    }    
 }
