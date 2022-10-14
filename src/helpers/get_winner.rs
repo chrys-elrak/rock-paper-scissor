fe30afd72b1400997b99ecc3de1a03b4baeaff5f
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-14 12:11:19 +0300

    translation

--- src/helpers/get_winner.rs
+++ src/helpers/get_winner.rs
@@ -6,6 +6,6 @@ pub fn message(winner: Option<bool>, lang: &Lang) {
     match winner {
         Some(true) => println!("{}", lang.win.green()),
         Some(false) => println!("{}", lang.lose.red()),
-        None =>  println!("{}", lang.draw.yellow()),
+        None =>  println!("{}", lang.draw_message.yellow()),
     }
 }
