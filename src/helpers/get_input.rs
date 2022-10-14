b32f270e01dd4a26aee82efcb745a4b9a06b6ce9
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-14 09:07:57 +0300

    update UI

--- src/helpers/get_input.rs
+++ src/helpers/get_input.rs
@@ -1,7 +1,8 @@
 use std::{ io::{self, Write} };
+use colored::Colorize;
 
-pub fn input(message: &'static str) -> String {
-    print!("{}: ", message);
+pub fn input(message: &str) -> String {
+    print!("\n{}\n>>> ", message.on_bright_green());
     io::stdout().flush().expect("");
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap_or_default();
