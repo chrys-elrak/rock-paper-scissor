e72f25bcc1537cf32410cbe436c8cada1e4aa9ae
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-14 09:56:09 +0300

    colorize print

--- src/models/stats.rs
+++ src/models/stats.rs
@@ -1,8 +1,10 @@
 use std::fmt::Display;
 
+use colored::Colorize;
+
 use super::lang::Lang;
 
-#[derive(Default, Debug)]
+#[derive(Default, Clone)]
 pub struct Stats {
     pub user: usize,
     pub computer: usize,
@@ -12,24 +14,25 @@ pub struct Stats {
 
 impl Display for Stats {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
+        let t = format!(
             "{}: {} - {}: {} - {}: {}",
             self.lang.you, self.user, self.lang.computer, self.computer, self.lang.draw, self.draws
         )
+        .bright_magenta();
+        write!(f, "{}", t)
     }
 }
 
 impl Stats {
-    pub fn update_stats(&mut self, result: Option<bool>){
+    pub fn update_stats(&mut self, result: Option<bool>) {
         match result {
             Some(true) => self.user += 1,
             Some(false) => self.computer += 1,
             None => self.draws += 1,
         }
     }
-    
+
     pub fn show(self) {
-        println!("{}", self);
+        println!("\n{}\n", self);
     }
 }
